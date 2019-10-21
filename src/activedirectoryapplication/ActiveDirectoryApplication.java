d1160afc8ae16f1440b2e8ed178c4a9611927648
scarroll <scarroll@NYWSSCARROLL.topaz.local>
2019-10-20 20:47:08 -0400

    Added CreateUser GUI and CreateUser Function

--- src/activedirectoryapplication/ActiveDirectoryApplication.java
+++ src/activedirectoryapplication/ActiveDirectoryApplication.java
@@ -5,17 +5,104 @@
  */
 package activedirectoryapplication;
 
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
 /**
  *
  * @author scarroll
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
+
+
+
+
 public class ActiveDirectoryApplication {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+    
+        public static void CreateUser() throws NamingException {
+        
+            
+         DirContext ctx = AuthenticateAD();
+        Attributes attrs = new BasicAttributes(true);
+        Attribute oc = new BasicAttribute("objectclass");
+        oc.add("top");
+        oc.add("person");
+        oc.add("organizationalPerson");
+        oc.add("user");
+        attrs.put(oc);
+        attrs.put(new BasicAttribute("cn", "testuser2"));
+        attrs.put(new BasicAttribute("name","test"));
+        ctx.createSubcontext("CN=testuser2,OU=Testusers,DC=lab,DC=home", attrs);
+        ctx.close();
+    
+    
+    
+    
+    
+    
+        }
+    
+    
+    
+    
+    
+    public static DirContext AuthenticateAD() throws NamingException
+{
+Hashtable<String, String> ldapEnv;
+ldapEnv = new Hashtable<String, String>(11);
+ldapEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+//ldapEnv.put(Context.PROVIDER_URL,  "ldap://WIN-4H1ILULLH7H.l:3
+ldapEnv.put(Context.PROVIDER_URL,  "ldap://192.168.1.20:389");
+ldapEnv.put(Context.SECURITY_AUTHENTICATION, "simple");
+//ldapEnv.put(Context.SECURITY_PRINCIPAL, "cn=administrateur,cn=users,dc=societe,dc=fr");
+ldapEnv.put(Context.SECURITY_PRINCIPAL, "jdoe@lab.home");
+ldapEnv.put(Context.SECURITY_CREDENTIALS, "Password1234");
+       
+            //ldapEnv.put(Context.SECURITY_PROTOCOL, "ssl");
+//ldapEnv.put(Context.SECURITY_PROTOCOL, "simple");
+DirContext ctx = new InitialDirContext(ldapEnv);
+ return ctx;
+       
+
+
+
+
+
+}
+
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) throws NamingException {
         // TODO code application logic here
+        
+        
+          CreateUser();
+          
+          
     }
     
 }
